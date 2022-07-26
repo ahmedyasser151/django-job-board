68eea3456f3482e1246c0c1e70d847b55d1dcae2
Ahmed Yasser <madamido151@gmailcom>
2022-07-26 17:21:42 +0200

    add job models & urls & views

--- job/models.py
+++ job/models.py
@@ -1,3 +1,27 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+JOP_TYPE = (
+    ('Part time','Part time'),
+    ('Full time','Full time')
+)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=10, choices=JOP_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def  __str__(self) -> str:
+        return self.name
--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from . import views
 
-# urlpatterns = [
-#     path('',),
-# ]
+urlpatterns = [
+    path('',views.jobs_list),
+    path('<int:id>',views.job_detail),
+]
